62a0db4f943e88e8eef3187217939d1a5c41b995
Daniel Martins Cabanas <danielmartinscabanas@gmail.com>
2022-09-22 00:42:46 -0300

    I created the data_tools.py, and docstrings

---
+++ data/data_tools.py
@@ -0,0 +1,175 @@
+import numpy as np
+import pandas as pd
+import stats
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+def regularize_dataframe(df1, df2):
+    """Function that forces the two dataframes to have the same lenght
+
+    Parameters
+    ----------
+        df1 : pandas.DataFrame
+            The first dataframe
+        df2 : pandas.DataFrame
+            The second fataframe
+
+    Returns
+    -------
+        df1 : pandas.DataFrame
+            A the first regularized dataframe
+        df2 : pandas.DataFrame
+            A the second regularized dataframe
+    """
+    _df1_cols = df1.columns.tolist()
+    _df2_cols = df2.columns.tolist()
+    _temp_df = pd.concat([df1, df2], axis=1,  join="inner")
+    df1 = _temp_df[_df1_cols]
+    df2 = _temp_df[_df2_cols]
+    return df1, df2
+
+def gen_resamples(sample, replicates=1):
+    """Generate random resamples of the given sample
+    
+    Parameters
+    ----------
+        sample : list, pandas.Series, pandas.Dataframe, numpy.ndarray
+            Data you want to randomly resample
+        replicates : int, optional
+            The number of random resamples you want to replicate (default is 
+            one)
+
+    Returns
+    -------
+        list[list[Any]]
+            The data randomly resampled
+    
+    """
+    if not isinstance(sample, list):
+        if isinstance(sample, pd.Series):
+            sample = sample.tolist()
+        elif isinstance(sample, np.ndarray):
+            sample = sample.tolist()
+        else:
+            sample = sample.iloc[:, 0].values.tolist()
+
+    _len_sample = len(sample)
+    rand_idxs = np.random.randint(_len_sample, size = (replicates, _len_sample))
+    resampled_sample = []
+    for lst in rand_idxs:
+        _resample = []
+        for i in lst:
+            _resample.append(sample[i])
+        resampled_sample.append(_resample)
+
+    return resampled_sample
+
+def confidence_interval(sample, confidence=0.95):
+    """Function that returns the confidence interval for any kind of that, even
+    if its required to use non-parametric methods
+
+    Parameters
+    ----------
+        sample : list, pandas.Series, pandas.Dataframe, numpy.ndarray
+            The sample you want to estimate the confidence interval
+        confidence : float, optional
+            The confidence you require for the calculus of the confidence
+            interval (default is 0.95)
+
+    Returns
+    -------
+        tuple
+            Three values in a tuple, where the first value is the lower limit,
+            the center value is the average of the sample, and the top value is 
+            the upper limit
+    
+    """
+    if not isinstance(sample, list):
+        if isinstance(sample, pd.Series):
+            sample = sample.tolist()
+        elif isinstance(sample, np.ndarray):
+            sample = sample.tolist()
+        else:
+            sample = sample.iloc[:, 0].values.tolist()
+
+    shapiro_test = stats.shapiro(sample)
+    # When its a normal series
+    if shapiro_test[1] > 0.05:
+        a = 1.0 * np.array(sample)
+        n = len(a)
+        m, se = np.mean(a), stats.sem(a)
+        h = se * stats.t.ppf((1 + confidence) / 2., n-1)
+        return m-h, m, m+h
+    # When the sample is not normal we are going to use a non-parametric method
+    # to find the confidence interval 
+    else:
+        _len_sample = len(sample)
+        sorted_sample = sample.sort()
+        lower = sample[round(_len_sample*((1-confidence)/2))-1]
+        average = sum(sample)/_len_sample
+        upper = sample[round(_len_sample*(confidence + (1-confidence)/2))-1]
+        return lower, average, upper
+
+def plot_normal(sample):
+    """Function that plot the histogram of the sample and the normal distribution
+adjusted 
+
+    Parameters
+    ----------
+        sample : list, pandas.Series, pandas.Dataframe, numpy.ndarray
+            The sample you want to plot
+
+    Returns
+    -------
+        None
+    """
+
+    """if not isinstance(sample, pd.DataFrame):
+        sample = pd.Series(sample).to_frame().to_numpy()"""
+    mu, std = norm.fit(sample)
+
+    if not isinstance(sample, np.ndarray):
+        if isinstance(sample, pd.Series):
+            sample = sample.to_numpy()
+        elif isinstance(sample, list):
+            sample = np.array(sample)
+        else:
+            sample = sample.iloc[:, 0].values.to_numpy()
+    mu, std = norm.fit(sample)
+
+    # Plotting the Histogram
+    plt.hist(sample, bins=35, density=True)
+
+    # Plotting the CDF
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std)
+
+    plt.plot(x, p, 'k', linewidth=2, color='red')
+    # Verifying if the sample is normal by Shapiro Wilk's test
+    shapiro_test = stats.shapiro(sample)
+    if shapiro_test[1] > 0.05:
+        title = f'A série (x̄={round(mu*100, 2)}%, s={round(std*100, 2)}%) é normal com {round(shapiro_test[0]*100, 2)}% de significância'
+    else:
+        title = f'A série (x̄={round(mu*100, 2)}%, s={round(std*100, 2)}%) não é normal com {round(shapiro_test[0]*100, 2)}% de significância'
+    plt.title(title)
+
+    plt.show()
+
+def pretty_print_dict(d, indent=0):
+    """Just a fucntion to print dictionaries more beautifully
+    Parameters
+    ----------
+        d : dict
+            A dictionary you want to print
+    
+    Returns
+    -------
+        None
+    """
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        if isinstance(value, dict):
+            pretty_print_dict(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
--- models/linear_models.py
+++ models/linear_models.py
@@ -1,37 +1,30 @@
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
-
-def _regularize_dataframe(df1, df2):
-    """Function that forces the two dataframes to have the same lenght
-
-    Parameters
-    ----------
-        df1 : pandas.DataFrame
-            The first dataframe
-        df2 : pandas.DataFrame
-            The second fataframe
-    
-    Returns
-    -------
-        df1 : pandas.DataFrame
-            A the first regularized dataframe
-        df2 : pandas.DataFrame
-            A the second regularized dataframe
-    """
-    _df1_cols = df1.columns.tolist()
-    _df2_cols = df2.columns.tolist()
-    _temp_df = pd.concat([df1, df2], axis=1,  join="inner")
-    df1 = _temp_df[_df1_cols]
-    df2 = _temp_df[_df2_cols]
-    return df1, df2
+import stats
+from data.data_tools import *
 
 class linearModel:
     def __init__(self, returns, factors):
-        self.returns, self.factors = _regularize_dataframe(returns, factors)
+        self.returns, self.factors = regularize_dataframe(returns, factors)
         self.tickers = self.returns.columns.values.tolist()
 
     def complete(self, X_p):
+        """Get all the complete analysis for a linear models. Including the coefi-
+        cients, the score and the prediction for each ticker
+
+        Parameters
+        ----------
+            X_p : list[list]
+                The list of the prediction of the factors
+        
+        Returns
+        -------
+            dict
+                A dictionary containing the coeficicients of the multivariate
+                linear regression, the score and the prediction for each given
+                ticker
+        """
         self.X_p = X_p
         self.results = {ticker:{'prediction':None,
                                 'coefs':None,
@@ -47,6 +40,21 @@ class linearModel:
         return self.results
 
     def predict(self, X_p):
+        """The prediction for each ticker based on the multivariate linear re-
+        gression fitted using all the given factors
+
+
+        Parameters
+        ----------
+            X_p : list[list]
+                The list of the prediction of the factors
+        
+        Returns
+        -------
+            dict
+                A dictionary containing the predicion of each ticker based on
+                the prediction of the factors
+        """
         self.X_p = X_p
         self.predictions = {ticker:{'prediction':None} for ticker in self.tickers}
         self._model = linear_model.LinearRegression(fit_intercept=True)
@@ -58,6 +66,18 @@ class linearModel:
         return self.predictions
 
     def coefs(self):
+        """The coefficients of the multivariate linear regression
+        
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            dict
+                A dictionary containing the coefficients of the multivariate 
+                linear regression fitted
+        """
         self.coefs = {ticker:{'coef':None} for ticker in self.tickers}
         self._model = linear_model.LinearRegression(fit_intercept=True)
         X = self.factors
@@ -67,7 +87,78 @@ class linearModel:
             self.coefs[ticker]['coef'] = np.insert(lm.coef_, 0, lm.intercept_)
         return self.coefs
 
+    def coefs_ci(self, X_p, n_boot=1000, ci=0.95):
+        """The confidence interval of coefficients of the multivariate linear 
+        regression using the boostrapping method
+        
+        Parameters
+        ----------
+            X_p : list[list]
+                The list of the prediction of the factors
+
+            n_boot : int, optional
+                Number of the resamples the algorithm is going to perform the
+                bootstrap method
+            ci : float, optional
+                Confidence interval
+
+        Returns
+        -------
+            dict
+                A dictionary containing the coefficients of the multivariate 
+                linear regression fitted, the score, the predicition of the 
+                coefficients for each ticker"""
+
+
+        tickers = self.returns.columns.values.tolist()
+        results = {ticker:{'prediction':None,
+                        'ci_coefs':None,
+                        'score':None} for ticker in tickers}
+        lm = linear_model.LinearRegression(fit_intercept=True)
+        ci_coefs = []
+        ci_intercepts = []
+        for ticker in tickers:
+            X = self.factors
+            Y = self.returns[ticker]
+            model = lm.fit(X.values,Y)
+            prediction = lm.predict(X_p)
+            coefs = lm.coef_
+            intercept = lm.intercept_
+            score = lm.score(X.values, Y)
+            results[ticker]['prediction'] = prediction[0]
+            results[ticker]['score'] = score
+            E = [Y[i] - lm.predict([x])[0] for i, x in enumerate(X.values.tolist())]
+            dict_boot = {'ticker':ticker}
+            for i in range(len(self.factors.columns)+1):
+                dict_boot['B'+str(i)] = []
+            for b in range(n_boot):
+                E_b = gen_resamples(E)[0]
+                Y_b = [lm.predict([x]) + E_b[i] for i, x in enumerate(X.values.tolist())]
+                lm_ = linear_model.LinearRegression(fit_intercept=True)
+                model_ = lm_.fit(X.values, Y_b)
+                dict_boot['B0'].append(lm_.intercept_[0])
+                for i, Bi in enumerate(lm_.coef_[0].tolist()):
+                    dict_boot['B'+str(i+1)].append(Bi)
+            for coef in list(dict_boot.keys())[-(len(self.factors.columns)+1):]:
+                coef_ci = confidence_interval(dict_boot[coef], confidence=ci)
+                dict_boot[coef] = coef_ci
+            results[ticker]['ci_coefs'] = {factor : dict_boot[factor] for factor in list(dict_boot.keys())[-(len(self.factors.columns)+1):]}
+
+        return results
+
     def scores(self):
+        """The score of the multivariate linear regression
+        
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            dict
+                A dictionary containing the scores of the multivariate linear 
+                regression fitted
+        """
         self.scores = {ticker:{'score':None} for ticker in self.tickers}
         self._model = linear_model.LinearRegression(fit_intercept=True)
         X = self.factors
